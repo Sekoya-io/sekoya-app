c499d9cebfec2b91fe8cd5c4b65bb62d078eef71
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2026-07-17 11:14:13 +0200

    F4858 · Menu : ajustements pictogramme simulation

--- sekoya-front/src/main/java/sekoya/front/common/template/MainTemplate.java
+++ sekoya-front/src/main/java/sekoya/front/common/template/MainTemplate.java
@@ -119,7 +119,7 @@ public abstract class MainTemplate extends AbstractWebPageTemplate {
         SekoyaApplication.get()
             .getHomePageLinkDescriptor()
             .navigationMenuItem(new ResourceModel("navigation.home"))
-            .iconClasses(Model.of("fa fa-fw fa-map-location-dot")),
+            .iconClasses(Model.of("fa fa-fw fa-map")),
         OrganisationListPage.linkDescriptor()
             .navigationMenuItem(new ResourceModel("navigation.organisation"))
             .iconClasses(Model.of("fa fa-fw fa-building")),
