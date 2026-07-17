a5f89336c150dd7c29061b5b4f72e728b53201be
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2026-07-17 14:25:11 +0200

    F4867 · Simulation > Offcanevas : processus triés par risque (desc)

--- sekoya-front/src/main/java/sekoya/front/simulation/component/SimulationSiteOffcanvasSitePanel.java
+++ sekoya-front/src/main/java/sekoya/front/simulation/component/SimulationSiteOffcanvasSitePanel.java
@@ -1,5 +1,6 @@
 package sekoya.front.simulation.component;
 
+import com.google.common.collect.ImmutableList;
 import igloo.wicket.behavior.ClassAttributeAppender;
 import igloo.wicket.component.CoreLabel;
 import igloo.wicket.component.EnclosureContainer;
@@ -7,6 +8,8 @@ import igloo.wicket.component.PlaceholderContainer;
 import igloo.wicket.condition.Condition;
 import igloo.wicket.markup.html.panel.GenericPanel;
 import igloo.wicket.model.BindingModel;
+import igloo.wicket.model.Models;
+import java.util.Comparator;
 import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -16,9 +19,10 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.iglooproject.wicket.more.markup.repeater.collection.CollectionView;
-import org.iglooproject.wicket.more.model.GenericEntityModel;
+import org.javatuples.Pair;
 import sekoya.back.business.common.model.atomic.Risque;
 import sekoya.back.business.processus.model.Processus;
+import sekoya.back.business.processus.service.IProcessusService;
 import sekoya.back.business.simulation.dto.SimulationSearchDto;
 import sekoya.back.business.simulation.service.business.ISimulationCalculService;
 import sekoya.back.business.site.model.Site;
@@ -30,6 +34,8 @@ public class SimulationSiteOffcanvasSitePanel extends GenericPanel<Site> {
 
   private static final long serialVersionUID = 1L;
 
+  @SpringBean private IProcessusService processusService;
+
   @SpringBean private ISimulationCalculService simulationCalculService;
 
   public SimulationSiteOffcanvasSitePanel(
@@ -38,6 +44,21 @@ public class SimulationSiteOffcanvasSitePanel extends GenericPanel<Site> {
       IModel<Processus> processusModel,
       IModel<SimulationSearchDto> simulationSearchDtoModel) {
     super(id, siteModel);
+
+    var processusRisqueModel =
+        LoadableDetachableModel.of(
+            () ->
+                siteModel.getObject().getProcessusEnabled().stream()
+                    .map(
+                        processus ->
+                            Pair.with(
+                                processus.getId(),
+                                simulationCalculService.getProcessusRisqueBrut(
+                                    processus, simulationSearchDtoModel.getObject())))
+                    .sorted(
+                        Comparator.comparingInt((Pair<Long, Risque> p) -> p.getValue1().getScore())
+                            .reversed())
+                    .collect(ImmutableList.toImmutableList()));
 
     add(
         new EnclosureContainer("processusContainer")
@@ -48,37 +69,35 @@ public class SimulationSiteOffcanvasSitePanel extends GenericPanel<Site> {
                         Bindings.simulationSearchDto().enableProcessus())))
             .add(
                 new CollectionView<>(
-                    "processus",
-                    BindingModel.of(siteModel, Bindings.site().processusEnabled()),
-                    GenericEntityModel.factory()) {
+                    "processus", processusRisqueModel, Models.serializableModelFactory()) {
 
                   @Override
-                  protected void populateItem(Item<Processus> item) {
-                    IModel<Risque> risqueModel =
+                  protected void populateItem(Item<Pair<Long, Risque>> item) {
+                    IModel<Processus> itemProcessusModel =
                         LoadableDetachableModel.of(
-                            () ->
-                                simulationCalculService.getProcessusRisqueBrut(
-                                    item.getModelObject(), simulationSearchDtoModel.getObject()));
+                            () -> processusService.getById(item.getModelObject().getValue0()));
 
                     item.add(
                             new WebMarkupContainer("icon")
                                 .add(
                                     new ClassAttributeAppender(
                                         BindingModel.of(
-                                            item.getModel(),
+                                            itemProcessusModel,
                                             Bindings.processus().thematique().iconCssClass()))),
-                            new CoreLabel("processus", item.getModel()).showPlaceholder(),
+                            new CoreLabel("processus", itemProcessusModel).showPlaceholder(),
                             new CoreLabel(
                                     "thematique",
                                     BindingModel.of(
-                                        item.getModel(), Bindings.processus().type().thematique()))
+                                        itemProcessusModel,
+                                        Bindings.processus().type().thematique()))
                                 .showPlaceholder(),
-                            new ScoreMonoValueRatingDisplayPanel<>("risque", risqueModel))
+                            new ScoreMonoValueRatingDisplayPanel<>(
+                                "risque", () -> item.getModelObject().getValue1()))
                         .add(
                             new AjaxEventBehavior("click") {
                               @Override
                               protected void onEvent(AjaxRequestTarget target) {
-                                processusModel.setObject(item.getModelObject());
+                                processusModel.setObject(itemProcessusModel.getObject());
                                 target.addChildren(
                                     getPage(), SimulationSiteOffcanvasBreadcrumbPanel.class);
                                 target.addChildren(
@@ -87,20 +106,18 @@ public class SimulationSiteOffcanvasSitePanel extends GenericPanel<Site> {
                             });
                   }
                 }.setItemReuseStrategy(ReuseIfModelsEqualStrategy.getInstance())
-                    .add(
-                        Condition.collectionModelNotEmpty(
-                                BindingModel.of(siteModel, Bindings.site().processusEnabled()))
-                            .thenShow()),
+                    .add(Condition.collectionModelNotEmpty(processusRisqueModel).thenShow()),
                 new PlaceholderContainer("placeholder")
-                    .condition(
-                        Condition.collectionModelNotEmpty(
-                            BindingModel.of(siteModel, Bindings.site().processusEnabled())))
+                    .condition(Condition.collectionModelNotEmpty(processusRisqueModel))
                     .add(ProcessusSiteAddPage.MAPPER.map(siteModel).link("add"))));
 
     add(
         new SimulationSiteOffcanvasSiteAleasGeographiquesPanel(
             "aleasGeographiques", siteModel, simulationSearchDtoModel));
 
-    add(Condition.modelNotNull(processusModel).negate().thenShowInternal());
+    add(
+        Condition.and(
+                Condition.modelNotNull(siteModel), Condition.modelNotNull(processusModel).negate())
+            .thenShowInternal());
   }
 }
